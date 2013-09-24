80babf9609a46b3d076c2aa5f880e7912aa8ed99
Colin Blackburn <colin@ximenes.org.uk>
2013-09-24 15:14:22 +0100

    Most tests duplicated in OmeroJava.
    All these tests must have originally come from OmeroJava as the lines
    still have trailing semicolons.

--- components/tools/OmeroPy/test/integration/test_clientusage.py
+++ components/tools/OmeroPy/test/integration/test_clientusage.py
@@ -22,59 +22,20 @@ class TestClientUsage(lib.ITest):
     All others should use the new_client(user=) syntax from lib.ITest
     """
 
-    def testClientClosedAutomatically(self):
-        client = omero.client()
-        client.createSession();
-        client.getSession().closeOnDestroy()
+    #def testClientClosedAutomatically(self): # -> ClientUsageTest
 
+    # Move to ClientUsageTest (it clearly originally came from there!)
     def testClientClosedManually(self):
         client = omero.client()
         client.createSession();
         client.getSession().closeOnDestroy();
         client.closeSession();
 
-    def testUseSharedMemory(self):
-        client = omero.client()
-        client.createSession();
+    #def testUseSharedMemory(self): # -> ClientUsageTest
 
-        self.assertEquals(0, len(client.getInputKeys()))
-        client.setInput("a", rstring("b"));
-        self.assertEquals(1, len(client.getInputKeys()))
-        self.assertTrue("a" in client.getInputKeys())
-        self.assertEquals("b", client.getInput("a").getValue());
+    #def testCreateInsecureClientTicket2099(self): # -> ClientUsageTest
 
-        client.closeSession();
-
-    def testCreateInsecureClientTicket2099(self):
-        secure = omero.client();
-        self.assert_(secure.isSecure())
-        try:
-            secure.createSession().getAdminService().getEventContext();
-            insecure = secure.createClient(False);
-            try:
-                insecure.getSession().getAdminService().getEventContext();
-                self.assert_( not insecure.isSecure());
-            finally:
-                insecure.closeSession();
-        finally:
-            secure.closeSession();
-
-    def testGetStatefulServices(self):
-        root = self.root
-        sf = root.sf
-        sf.setSecurityContext(omero.model.ExperimenterGroupI(0, False))
-        sf.createRenderingEngine()
-        srvs = root.getStatefulServices()
-        self.assertEquals(1, len(srvs))
-        try:
-            sf.setSecurityContext(omero.model.ExperimenterGroupI(1, False))
-            self.fail("Should not be allowed")
-        except:
-            pass # good
-        srvs[0].close()
-        srvs = root.getStatefulServices()
-        self.assertEquals(0, len(srvs))
-        sf.setSecurityContext(omero.model.ExperimenterGroupI(1, False))
+    #def testGetStatefulServices(self): # -> ClientUsageTest
 
 if __name__ == '__main__':
     unittest.main()
